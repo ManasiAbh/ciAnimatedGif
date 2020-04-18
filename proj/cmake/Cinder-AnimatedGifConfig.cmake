7f6c8d8433f6a4a495766bbe1702b07c8bad2ff5
Manasi Abhyankar <manasia2@illinois.edu>
2020-04-18 13:35:46 -0500

    Edited the cmake file.

--- proj/cmake/Cinder-AnimatedGifConfig.cmake
+++ proj/cmake/Cinder-AnimatedGifConfig.cmake
@@ -1,24 +1,24 @@
-if(NOT TARGET Cinder-Notifications)
+if(NOT TARGET ciGif)
     # Define ${Cinder-Notifications_PROJECT_ROOT}. ${CMAKE_CURRENT_LIST_DIR} is just the current directory.
-    get_filename_component(Cinder-Notifications_PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
+    get_filename_component(ciGif_PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
 
     # Define ${CINDER_PATH} as usual.
-    get_filename_component(CINDER_PATH "${Cinder-Notifications_PROJECT_ROOT}/../.." ABSOLUTE)
+    get_filename_component(CINDER_PATH "${ciGif_PROJECT_ROOT}/../.." ABSOLUTE)
 
     # Make a list of source files and define that to be ${SOURCE_LIST}.
     file(GLOB SOURCE_LIST CONFIGURE_DEPENDS
-            "${Cinder-Notifications_PROJECT_ROOT}/src/rph/NotificationManager.cpp"
-            "${Cinder-Notifications_PROJECT_ROOT}/src/rph/Notification.cpp"
+            "${ciGif_PROJECT_ROOT}/src/ciAnimatedGif.cpp"
+            "${ciGif_PROJECT_ROOT}/src/ciAnimatedGif.h"
             )
 
     # Create the library!
-    add_library(Cinder-Notifications ${SOURCE_LIST})
+    add_library(ciGif ${SOURCE_LIST})
 
     # Add include directories.
     # Notice that `cinderblock.xml` has `<includePath>src</includePath>`.
     # So you need to set `../../src/` to include.
-    target_include_directories(Cinder-Notifications PUBLIC "${Cinder-Notifications_PROJECT_ROOT}/src" )
-    target_include_directories(Cinder-Notifications SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
+    target_include_directories(ciGif PUBLIC "${ciGif_PROJECT_ROOT}/src" )
+    target_include_directories(ciGif SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
 
 
     # If your Cinder block has no source code but instead pre-build libraries,
@@ -32,6 +32,6 @@ if(NOT TARGET Cinder-Notifications)
                 "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
                 "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}")
     endif()
-    target_link_libraries(Cinder-Notifications PRIVATE cinder)
+    target_link_libraries(ciGif PRIVATE cinder)
 
 endif()
